ac9f9588ecc732fe9612f86512ef6990a0107616
Andrew Guest <andrew@aguest.me>
2025-03-17 22:28:41 -0500

    Use environment variable

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from contextlib import asynccontextmanager
 
 import motor.motor_asyncio
@@ -20,9 +21,7 @@ templates = Jinja2Templates(directory="templates")
 
 # Set up the MongoDB connection
 async def init_db():
-    client = motor.motor_asyncio.AsyncIOMotorClient(
-        "mongodb+srv://read-only-user:AtW8XtX2TU5sHy8p@aguestmecluster.3tbfk.mongodb.net/?retryWrites=true&w=majority&appName=AguestmeCluster"
-    )
+    client = motor.motor_asyncio.AsyncIOMotorClient(os.getenv("MONGO_URI"))
     await init_beanie(database=client.aguest_me, document_models=[Projects])
 
 
@@ -55,9 +54,6 @@ app.add_middleware(
 async def index(request: Request):
     # Get the projects from the DB
     all_projects = await Projects.find_all().to_list()
-    print(all_projects)
-
-    print(dir(Projects))
 
     return templates.TemplateResponse(
         request=request, name="index.html", context={"projects": all_projects}
